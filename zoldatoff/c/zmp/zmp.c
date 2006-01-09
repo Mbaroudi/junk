162be63010766c5b5da151831db605500e96932d
zoldatoff <zoldatoff@8af51d34-cf08-0410-899b-c60a60f868da>
2006-01-09 01:28:52 +0000

    You can add selected files & folders to playlist. You can select playlist items.

--- zoldatoff/c/zmp/zmp.c
+++ zoldatoff/c/zmp/zmp.c
@@ -28,6 +28,7 @@
 #define TAB 0x09               	//Tab key
 #define SPACE ' '
 #define PG_UP 0x153
+#define PG_DOWN 0x152
 //#define LF 0x0a               //Line feed
 //#define BACKSPACE 0x08        //Backspace
 //#define SAVE 0x13             //Ctrl-S for Save
@@ -38,6 +39,7 @@
 struct filelist {
 	char f_name[MAX_FILE_NAME];
 	char f_type;
+	int  is_selected;
 };
 
 struct playlist {
@@ -47,6 +49,7 @@ struct playlist {
 	char artist[MAX_TAG];
 	char genre[MAX_TAG];
 	char album[MAX_TAG];
+	int  is_selected;
 };
 
 //================================================================================
@@ -68,16 +71,26 @@ void drawPlaylist(WINDOW *window,
 		int item, 
 		int max_item); 		
 
-void addtoPlaylist(struct playlist *play_list[],
+void addFile(struct playlist *play_list[],
 		struct filelist *dir_list[],
 		char current_dir[],
 		int explorer_item,
 		int max_playlist_item);
 
-void addSubfolders(char dir[], 
+void addFolder(char dir[], 
 		struct playlist *play_list[], 
 		int *max_item);
 
+void addtoPlaylist(struct filelist *dir_list[],
+		struct playlist *play_list[],
+		int explorer_item,
+		char current_dir[],
+		int *max_playlist_item);
+
+void delfromPlaylist(struct playlist *play_list[], 
+		int playlist_item, 
+		int *max_playlist_item);
+					
 char *upDir(char directory[]);
 
 int incItem(int item, int max_item);
@@ -114,7 +127,7 @@ int main(int argc, char *argv[])
         drawWindow(base_win, BASEWIN_COLOR, "Zoldatoff Media Player");
         explorer_win = newwin(maxy-2, maxx-2, 1, 1);
         drawWindow(explorer_win, EXPLORER_COLOR, "Explorer");
-        playlist_win = newwin(maxy-2, (3*maxx)/4 - 1, 1, maxx/4);
+        playlist_win = newwin(maxy-2, (3*maxx)/4, 1, maxx/4);
         drawWindow(playlist_win, PLAYLIST_COLOR, "Playlist");
         song_info_win = newwin(maxy-2, maxx/4 - 1, 1, 1);
         drawWindow(song_info_win, SONG_INFO_COLOR, "Song");
@@ -130,20 +143,21 @@ int main(int argc, char *argv[])
 	for (t=0; t<MAX_LIST; t++)
 		dir_list[t] = NULL;
 	
-        char current_dir[MAX_FILE_NAME]="/";
+        char current_dir[MAX_FILE_NAME]="/home/media/music/";
         int playlist_item=0, max_playlist_item=-1;
         int explorer_item=0, max_explorer_item=0, show_explorer=0;
 
         max_explorer_item = listDir(current_dir, dir_list);
 
         int key, i=2;  //i is unneeded!!
+	int playlist_sel = 0, explorer_sel = 0; 		//there are no selected items in explorer & playlist 
 	
         while (1) {
                 key = wgetch(playlist_win);
                 if (key=='q')
                         break;
                 switch (key) {
-                case KEY_DOWN: 		//scroll list down (done)
+		case KEY_DOWN: 		//DONE: scroll list down
                         if (show_explorer) {
                                 explorer_item = incItem(explorer_item, max_explorer_item);
 				drawExplorer(explorer_win,dir_list,explorer_item,max_explorer_item);
@@ -153,7 +167,7 @@ int main(int argc, char *argv[])
 				drawPlaylist(playlist_win,play_list,playlist_item,max_playlist_item);
 			}
 			break;
-                case KEY_UP: 		//scroll list up (done)
+		case KEY_UP: 		//DONE: scroll list up
                         if (show_explorer) {
                                 explorer_item = decItem(explorer_item, max_explorer_item);
 				drawExplorer(explorer_win,dir_list,explorer_item,max_explorer_item);
@@ -162,8 +176,28 @@ int main(int argc, char *argv[])
                                 playlist_item = decItem(playlist_item, max_playlist_item);
 				drawPlaylist(playlist_win,play_list,playlist_item,max_playlist_item);
 			}
-                        break;
-                case TAB: 		//switch between playlist & explorer (done)
+			break;
+                 case PG_DOWN:
+			if (show_explorer) {
+				explorer_item = (explorer_item<max_explorer_item-(maxy/2)) ? explorer_item+(maxy/2) : max_explorer_item;
+				drawExplorer(explorer_win,dir_list,explorer_item,max_explorer_item);
+			}
+			else {
+				playlist_item = (playlist_item<max_playlist_item-(maxy/2)) ? playlist_item+(maxy/2) : max_playlist_item;
+				drawPlaylist(playlist_win,play_list,playlist_item,max_playlist_item);
+			}
+			break;
+		case PG_UP:
+			if (show_explorer) {
+				explorer_item -= (explorer_item>(maxy/2)) ? (maxy/2) : explorer_item;
+				drawExplorer(explorer_win,dir_list,explorer_item,max_explorer_item);
+			}
+			else {
+				playlist_item -= (playlist_item>(maxy/2)) ? (maxy/2) : playlist_item;
+				drawPlaylist(playlist_win,play_list,playlist_item,max_playlist_item);
+			}
+			break;
+		case TAB: 		//DONE: switch between playlist & explorer
                         if (!show_explorer) {
                                 drawExplorer(explorer_win,dir_list,explorer_item,max_explorer_item);
                                 show_explorer=1;
@@ -175,49 +209,84 @@ int main(int argc, char *argv[])
                         }
                         break;
 		case SPACE: 		//TODO: mark files and playlist items
-                        mvwprintw(song_info_win,i,1,"Space");
-                        wrefresh(song_info_win);
-                        i++;
+			if (show_explorer) {
+				if ( dir_list[explorer_item]->is_selected ) {
+				       dir_list[explorer_item]->is_selected = 0;
+				       explorer_sel--;
+				}
+				else {
+				       dir_list[explorer_item]->is_selected = 1;
+				       explorer_sel++;
+				}
+				explorer_item = explorer_item<max_explorer_item ? ++explorer_item : max_explorer_item;
+				drawExplorer(explorer_win,dir_list,explorer_item,max_explorer_item);
+			}
+			else {
+				if ( play_list[playlist_item]->is_selected ) {
+				       play_list[playlist_item]->is_selected = 0;
+				       playlist_sel--;
+				}
+				else {
+				       play_list[playlist_item]->is_selected = 1;
+				       playlist_sel++;
+				}
+				playlist_item = playlist_item<max_playlist_item ? ++playlist_item : max_playlist_item;
+				drawPlaylist(playlist_win,play_list,playlist_item,max_playlist_item);
+			}
                         break;
                 case KEY_RIGHT: 	//as Enter....
-                case ENTER: 		//add to playlist //play selected song //go to folder
+		case ENTER: 		//add to playlist & play selected song //DONE: open folder
 			if (show_explorer && dir_list[explorer_item]) {
 				strcat(current_dir, dir_list[explorer_item]->f_name);
 				strcat(current_dir, "/");
         			max_explorer_item = listDir(current_dir, dir_list);
-				explorer_item=0;
+				explorer_item = explorer_sel = 0;
 				drawExplorer(explorer_win,dir_list,explorer_item,max_explorer_item);
 			}
                         break;
-                case KEY_LEFT: 		//go up one dir (done)
-		case PG_UP: 		//go up one dir (done)
+		case KEY_LEFT: 		//DONE: go up one dir
 			if (show_explorer) {
 				strcpy(current_dir, upDir(current_dir));
         			max_explorer_item = listDir(current_dir, dir_list);
-				explorer_item=0;
+				explorer_item = explorer_sel = 0;
 				drawExplorer(explorer_win,dir_list,explorer_item,max_explorer_item);
 			}
 			break;
 		case 'a': 		//add files to playlist
 			if (show_explorer && dir_list[explorer_item]) {
-				if (dir_list[explorer_item]->f_type == 'm') {
-					max_playlist_item++;
-					addtoPlaylist(play_list,dir_list,current_dir,explorer_item,max_playlist_item);
+				if (explorer_sel) {
+					int k;
+					for (k=0; k<=max_explorer_item; k++) {
+						if (dir_list[k]->is_selected)
+							addtoPlaylist(dir_list, play_list, k, current_dir, &max_playlist_item);	
+						dir_list[k]->is_selected = 0;
+					}
 				}
-
-				if (dir_list[explorer_item]->f_type == 'd') {
-					char tmp[MAX_FILE_NAME];
-					strcpy(tmp,current_dir);
-					strcat(tmp,dir_list[explorer_item]->f_name);
-					strcat(tmp,"/");
-					addSubfolders(tmp, play_list, & max_playlist_item);
-				}
+				else
+					addtoPlaylist(dir_list, play_list, explorer_item, current_dir, &max_playlist_item);	
 					
 				explorer_item = explorer_item<max_explorer_item ? ++explorer_item : max_explorer_item;
 				drawExplorer(explorer_win,dir_list,explorer_item,max_explorer_item);
+				explorer_sel = 0;
 			}
 			break;
 		case 'd': 		//delete items from playlist
+			if (!show_explorer && play_list[playlist_item]) {
+				if (playlist_sel) {
+					int k;
+					for (k=0; k<=max_playlist_item; k++) {
+						if (play_list[k]->is_selected)
+							delfromPlaylist(play_list, k, &max_playlist_item);	
+						play_list[k]->is_selected = 0;
+					}
+				}
+				else
+					delfromPlaylist(play_list, playlist_item, &max_playlist_item);
+			
+				//do something with playlist_item!!!	
+				drawPlaylist(playlist_win,play_list,playlist_item,max_playlist_item);
+				playlist_sel = 0;
+			}
 			break;
 		case '+': 		//volume up
 			break;
@@ -332,7 +401,7 @@ int listDir(const char *directory, struct filelist *dir_list[]) 	//the array of 
 				tmp[1] = 'd';
 			if (S_ISREG(buf->st_mode))
 				tmp[1] = 'f';
-			if (tmp[1]!='d') { 		//a VERY bad if....
+			if (tmp[1]!='d') { 		//a VERY bad if....i think
 				char *string;
 				string=& (ep->d_name[strlen(ep->d_name)-4]);
 				if ( (string[0]=='.') && (tolower(string[1])=='m') && (tolower(string[2])=='p') && (string[3]=='3') )
@@ -355,8 +424,8 @@ int listDir(const char *directory, struct filelist *dir_list[]) 	//the array of 
 }//listDir
 
 //================================================================================					 
-
-void addSubfolders(char dir[], 
+//Add folder to playlist
+void addFolder(char dir[], 
 		struct playlist *play_list[], 
 		int *max_item)
 {
@@ -366,7 +435,7 @@ void addSubfolders(char dir[],
 	for (i=0; i<=num; i++) {
 		if ( list[i]->f_type == 'm' ) {
 			(*max_item)++;
-			addtoPlaylist(play_list, list, dir, i, *max_item);
+			addFile(play_list, list, dir, i, *max_item);
 		}
 
 		if ( list[i]->f_type == 'd' ) {
@@ -374,7 +443,7 @@ void addSubfolders(char dir[],
 			strcpy(tmp,dir);
 			strcat(tmp,list[i]->f_name);
 			strcat(tmp,"/");
-			addSubfolders(tmp, play_list, max_item);
+			addFolder(tmp, play_list, max_item);
 		}
 	}
 }
@@ -399,20 +468,24 @@ void drawExplorer(WINDOW *window,
 			case 'd':
 				if (i==item) wattrset(window, COLOR_PAIR(EXPLORER_DIR_SEL_COLOR) | WA_BOLD);
 				else wattrset(window, COLOR_PAIR(EXPLORER_DIR_COLOR) | WA_BOLD);
-                		mvwprintw(window,i+1-tmp,1, "  /%s  ",dir_list[i]->f_name);
+				if (dir_list[i]->is_selected) mvwprintw(window,i+1-tmp,1, " </%s> ",dir_list[i]->f_name);
+				else mvwprintw(window,i+1-tmp,1, "  /%s  ",dir_list[i]->f_name);
 				break;
 			case 'f':
 				if (i==item) wattrset(window, COLOR_PAIR(EXPLORER_SEL_COLOR) | WA_BOLD);
 				else wattrset(window, COLOR_PAIR(EXPLORER_COLOR) | WA_BOLD);
-                		mvwprintw(window,i+1-tmp,1, "  %s  ",dir_list[i]->f_name);
+                		if (dir_list[i]->is_selected) mvwprintw(window,i+1-tmp,1, " <%s> ",dir_list[i]->f_name);
+				else mvwprintw(window,i+1-tmp,1, "  %s  ",dir_list[i]->f_name); 
 				break;
 			case 'm':
 				if (i==item) wattrset(window, COLOR_PAIR(EXPLORER_MEDIA_SEL_COLOR) | WA_BOLD);
 				else wattrset(window, COLOR_PAIR(EXPLORER_MEDIA_COLOR) | WA_BOLD);
-                		mvwprintw(window,i+1-tmp,1, "  %s  ",dir_list[i]->f_name);
+                		if (dir_list[i]->is_selected) mvwprintw(window,i+1-tmp,1, " <%s> ",dir_list[i]->f_name);
+				else mvwprintw(window,i+1-tmp,1, "  %s  ",dir_list[i]->f_name);
 				break;
 		}
-                mvwprintw(window,i+1-tmp,60,"  %c  ",dir_list[i]->f_type);
+                mvwprintw(window,i+1-tmp,60,"  %c  ",dir_list[i]->f_type);   //for debug only!!
+                mvwprintw(window,i+1-tmp,70,"  %d  ",dir_list[i]->is_selected);   //for debug only!!
         }
         redrawWindow(window);
 }//drawExplorer
@@ -456,16 +529,19 @@ void drawPlaylist(WINDOW *window,
                 else
                         wattrset(window, COLOR_PAIR(PLAYLIST_COLOR) | WA_BOLD);
 		
-                mvwprintw(window,i+1-tmp,1,"  %s  ",play_list[i]->f_name);
-                //mvwprintw(window,i+1-tmp,30,"%s",play_list[i]->path);
+		if (play_list[i]->is_selected)
+                	mvwprintw(window,i+1-tmp,1," <%s> ",play_list[i]->f_name);
+		else
+                	mvwprintw(window,i+1-tmp,1,"  %s  ",play_list[i]->f_name);
+                //mvwprintw(window,i+1-tmp,30,"%s",play_list[i]->path); //it's only for debug....
         }
         redrawWindow(window);
 }//drawPlaylist
 
 //================================================================================
 
-//Add files to playlist
-void addtoPlaylist(struct playlist *play_list[],
+//Add file to playlist
+void addFile(struct playlist *play_list[],
 		struct filelist *dir_list[],
 		char current_dir[],
 		int explorer_item,
@@ -477,7 +553,7 @@ void addtoPlaylist(struct playlist *play_list[],
 	strcpy(tmp,current_dir);
 	strcat(tmp,dir_list[explorer_item]->f_name);
 	strcpy(play_list[max_playlist_item]->path, tmp);
-}//addtoPlaylist
+}//addFile
 
 //================================================================================
 
@@ -499,4 +575,36 @@ void redrawWindow(WINDOW* window)
 {
 	touchwin(window);
 	wrefresh(window);
+}
+
+//================================================================================
+
+//Add file or folder to playlist
+void addtoPlaylist(struct filelist *dir_list[],
+		struct playlist *play_list[],
+		int explorer_item,
+		char current_dir[],
+		int *max_playlist_item)
+{
+	if (dir_list[explorer_item]->f_type == 'm') {
+		(*max_playlist_item)++;
+		addFile(play_list,dir_list,current_dir,explorer_item,*max_playlist_item);
+	}
+		
+	if (dir_list[explorer_item]->f_type == 'd') {
+		char tmp[MAX_FILE_NAME];
+		strcpy(tmp,current_dir);
+		strcat(tmp,dir_list[explorer_item]->f_name);
+		strcat(tmp,"/");
+		addFolder(tmp, play_list, max_playlist_item);
+	}
+}
+
+//================================================================================
+
+//Delete item from playlist
+void delfromPlaylist(struct playlist *play_list[], 
+		int playlist_item, 
+		int *max_playlist_item)
+{
 }
