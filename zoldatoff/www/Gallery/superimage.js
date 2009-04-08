659f5a2c80a01b99d07bbfee3f601b0af27d206e
appleapple <appleapple@8af51d34-cf08-0410-899b-c60a60f868da>
2009-04-08 18:52:54 +0000

    Gallery is based on class myImage

--- zoldatoff/www/Gallery/superimage.js
+++ zoldatoff/www/Gallery/superimage.js
@@ -26,3 +26,47 @@
 	this.subImg.onload = subOnTop;
 	this.mainImg.onload = mainOnTop;
  }
+ 
+ function getNumber(str) {
+ 	var pos_ = str.indexOf("_");
+ 	return str.substring(pos_+1, str.length);
+ }
+ 
+function onSubLoad() {
+		document.getElementById("bottomDiv_" + getNumber(this.id)).appendChild(this);
+		this.className = "img-show";
+		
+		if (this.height < this.parentNode.offsetHeight) {
+			this.style.marginTop = Math.round((this.parentNode.offsetHeight - this.height) / 2) + 'px';
+		}
+		else
+			this.style.marginTop = "0px";
+}
+
+function onMainLoad() {		
+		document.getElementById("sub_" + getNumber(this.id)).className = "img-hide";
+		document.getElementById("bottomDiv_" + getNumber(this.id)).appendChild(this);
+		this.className = "img-show";
+		
+		if (this.height < this.parentNode.offsetHeight) {
+				this.style.marginTop = Math.round((this.parentNode.offsetHeight - this.height) / 2) + 'px';
+			}
+		else
+			this.style.marginTop = "0px";
+			
+		this.onclick = function() { document.getElementById("mainImage").src = this.src; }
+}
+ 
+ function myImage(imgSrc, loadingSrc, imgNum){
+ 	this.mainImg = new Image();
+	this.subImg = new Image();
+	
+	this.subImg.src = loadingSrc;
+	this.mainImg.src = imgSrc;
+	
+	this.mainImg.id = "main_" + imgNum;
+	this.subImg.id = "sub_" + imgNum;
+		
+	this.subImg.onload = onSubLoad;
+	this.mainImg.onload = onMainLoad;
+}
