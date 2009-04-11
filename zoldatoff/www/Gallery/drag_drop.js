d0e2e859523236484bf4bd3ec45d43c1ad5d65a6
appleapple <appleapple@8af51d34-cf08-0410-899b-c60a60f868da>
2009-04-11 22:35:43 +0000

    Second d&d implemented. D&d is buggy in firefox...

--- zoldatoff/www/Gallery/drag_drop.js
+++ zoldatoff/www/Gallery/drag_drop.js
@@ -31,7 +31,7 @@ function initMove(event) {
     var event = event || window.event;
 	dragElement = event.target || ev.srcElement;
 	
-	if (dragElement.id == 'dragDiv') {
+	if (dragElement.id == 'dragDiv' || dragElement.id == "horizontal2") {
 		dragDirection = 'h';
 		x0 = defPosition(event).x;
 		y0 = defPosition(event).y;
@@ -49,9 +49,10 @@ function moveHandler(event) {
     var event = event || window.event;
     if (moveState &&
 		divX0 + defPosition(event).x - x0 > 50 &&
-		divX0 + defPosition(event).x - x0 < document.body.clientWidth - 50) 
+		divX0 + defPosition(event).x - x0 < document.body.clientWidth - 50 &&
+		dragElement.id == 'dragDiv')
 	{
-        dragElement.style.left = divX0 + defPosition(event).x - x0 + 'px'; //style.left		
+        dragElement.style.left = divX0 + defPosition(event).x - x0 + 'px';	
         
 		document.getElementById("leftDiv").style.width = dragElement.style.left;
 		document.getElementById("mainDiv").style.left = dragElement.style.left;
@@ -60,6 +61,32 @@ function moveHandler(event) {
 			document.getElementById("leftDiv").clientWidth - 
 			dragElement.clientWidth + 'px';
     }
+	
+	if (moveState &&
+		dragElement.id == "horizontal2")
+	{
+		dragElement.style.top = divY0 + defPosition(event).y - y0 + 'px';	
+		
+		document.getElementById("leftDiv").style.height = 
+			document.getElementById("horizontal2").offsetTop - 
+			document.getElementById("leftDiv").offsetTop + 'px';
+			
+		document.getElementById("mainDiv").style.height = document.getElementById("leftDiv").style.height;
+		document.getElementById("dragDiv").style.height = document.getElementById("leftDiv").style.height;
+			
+		document.getElementById("bottomDiv").style.top =
+			document.getElementById("horizontal2").offsetTop + 'px';
+			
+		document.getElementById("bottomDiv").style.height = 
+			window.innerHeight - 
+			document.getElementById("horizontal2").offsetTop -
+			document.getElementById("horizontal2").clientHeight + 'px';	
+			
+		placeImage(document.getElementById('leftImg'));	
+		placeImage(document.getElementById('mainImage'));	
+		for (var i = 0; i < 5; i++) placeImage( document.getElementById( "main_000" + (i+1) ) );
+		for (var i = 0; i < 5; i++) placeImage( document.getElementById( "sub_000" + (i+1) ) );
+	}
 }
 
 document.onmousedown = initMove;
