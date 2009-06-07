00029985f3adb414104987d1c0d1c8269fcabfa8
zoldatoff <zoldatoff@8af51d34-cf08-0410-899b-c60a60f868da>
2009-06-07 20:23:34 +0000

    Working on top browsers. Basic ajax implemented. Varios image sizes (thumbs & normal). Logging added.

--- zoldatoff/www/zGallery/trunk/js/zGallery.js
+++ zoldatoff/www/zGallery/trunk/js/zGallery.js
@@ -1,11 +1,14 @@
-function interface() {		
+var imageList = new Array();
+var myImg = new Array();
+var output;
+
+function setInterface() {		
 	maxX = $('body').width();
 	maxY = $('body').height();
 	
 	headerY = $('#header').height();
 	footerY = $('#footer').height();
 	thumbY = $('#thumbsDiv').height();
-	thumbX = $('#thumbsDiv').width();
 	captionY = $('#captionDiv').height();
 	leftX = $('#leftDiv').width();
 	rightX = $('#rightDiv').width();
@@ -17,22 +20,52 @@ function interface() {
 	$('#containerDiv').height(containerY+'px');
 	$('#imagesDiv').width(maxX-leftX-rightX+'px');
 	
+	thumbX = $('#thumbsDiv').width();
 	$('#thumbsDiv').css("left", (maxX-leftX-rightX-thumbX)/2 + 'px');
+	
+	errorLog("Page reformatted", "blue");
+	
+	$.getJSON('php/list_images.php', {gallery: 'default'}, fillImages);
+}
+
+function fillImages(data) {
+	myThumbs = $('#thumbs').empty();
+	
+	for (var i=0; (i<data.imagelist.length) && (i<10); i++){
+		myThumbs.append('<li class="lithumb" id="li' + i + '"> </li>');
+		myLi = $('#li'+i);
+		myLi.attr('id', 'li' + i);
+		
+		myLi.append('<img class="thumb" id="img' + i + '"');
+		imageList[data.imagelist[i].thumb_src] = data.imagelist[i];
+		myImg[i] = $('#img'+i);
+		myImg[i].number
+		myImg[i].attr('src', data.imagelist[i].thumb_src);
+		
+		//$(#caption).html(data.imagelist[i].title);
+		//myImg[i].attr('alt', data.imagelist[i].title);
+		//myImg[i].attr('title', data.imagelist[i].title);
+	}
 	
 	$('img.thumb').load(function(){
 		$(this).click(function(){
 			var mysrc = $(this).attr('src');
-			$('#image').attr('src', mysrc);
+			$('#image').attr('src', imageList[mysrc].norm_src);
 			$('#image').css('display','none').fadeIn(1000);
 		})
 	})
 	
-/*
-	$('#image').load(function(){
-		alert($(this).height());
-	})
-*/
+	$('#thumbsDiv').css("left", (maxX-leftX-rightX-data.imagelist.length*78)/2 + 'px');
 }
 
-$(document).ready(interface);
-$(document).resize(interface);
+$(document).ready(setInterface);
+$(document).resize(setInterface);
+
+function errorLog(message, color) {
+	var currentTime = new Date();
+  	var hours = currentTime.getHours();
+  	var min = currentTime.getMinutes();
+	var sec = currentTime.getSeconds();
+
+	$('#rightDiv').append('<span class="log" style="color:' + color + '">[' + hours + ':' + min + ':' + sec + '] ' + message + '</span>');
+}
--- zoldatoff/www/zGallery/trunk/php/list_images.php
+++ zoldatoff/www/zGallery/trunk/php/list_images.php
@@ -3,18 +3,18 @@
 		public $thumb_src = 'img/thumb/00.jpg';
 		public $norm_src = 'img/norm/00.jpg';
 		public $full_src = 'img/full/00.jpg';
+		public $number = 0;
 		
-		function __construct($img_name) {
+		function __construct($img_name, $n) {
 			$this->thumb_src = 'img/thumb/' . $img_name;
 			$this->norm_src = 'img/norm/' . $img_name;
 			$this->full_src = 'img/full/' . $img_name;
+			$this->number = $n;
 		}
 	}
 	
-	//$img = array();
-	
-    for($i=1;$i<=2;$i++)
-		$img[] = new image($i . 'jpg');
+    for($i=1;$i<=8;$i++)
+		$img[] = new image('0' . $i . '.jpg', i-1);
 		
 	echo '{"imagelist": ' . json_encode($img) . '}';
 ?>
