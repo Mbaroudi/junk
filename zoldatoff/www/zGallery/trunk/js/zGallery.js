e422c1e6aa938dfbb5b38a4546277a524a577750
zoldatoff <zoldatoff@8af51d34-cf08-0410-899b-c60a60f868da>
2009-06-12 17:11:42 +0000

    Small code fixes & improvements. TODO: 1) Resize 2) Scroll thumbs 3) Genres and albums 4) Image loader 5) Admin interface 6) Google gears

--- zoldatoff/www/zGallery/trunk/js/zGallery.js
+++ zoldatoff/www/zGallery/trunk/js/zGallery.js
@@ -14,15 +14,15 @@ function setInterface() {
 	captionY = $('#captionDiv').height();
 	leftX = $('#leftDiv').width();
 	rightX = $('#rightDiv').width();
+	thumbX = $('#thumbsDiv').width();
 	wrapY = maxY-headerY-footerY;
 	containerY = wrapY - thumbY - captionY;
 	wrapperY = containerY - captionY;
 	
+	//Позиционируем элементы на странице
 	$('#wrapDiv').height(wrapY+'px');
 	$('#containerDiv').height(containerY+'px');
 	$('#imagesDiv').width(maxX-leftX-rightX+'px');
-	
-	thumbX = $('#thumbsDiv').width();
 	$('#thumbsDiv').css("left", (maxX-leftX-rightX-thumbX)/2 + 'px');
 	
 	errorLog("Page reformatted", "blue");
@@ -34,6 +34,7 @@ function setInterface() {
 function fillImages(data) {
 	var myThumbs = $('#thumbs').empty();
 	
+	//Отображаем thumbnail-ы
 	for (var i=0; (i<data.imagelist.length) && (i<10); i++){
 		myThumbs.append('<li class="lithumb" id="li' + i + '"/>');
 		$('#li'+i).append('<img class="thumb" id="img' + i + '"/>');
@@ -41,12 +42,11 @@ function fillImages(data) {
 		imageList[i] = data.imagelist[i];
 		$('#img'+i).attr('src', data.imagelist[i].thumb_src);
 		$('#img'+i).attr('number', i);
-		
-		//$(#caption).html(data.imagelist[i].title);
 	}
 	
-	errorLog("Request received", "red");
+	errorLog("Request received: " + data.imagelist.length + " items", "red");
 	
+	//Добавляем обработчик кликов для каждого из thumbnails-ов
 	$('img.thumb').load(function(){
 		$(this).click(function(){
 			var myNumber = $(this).attr('number');
@@ -57,23 +57,43 @@ function fillImages(data) {
 			$('img.activethumb').removeClass("activethumb");
 			$(this).addClass("activethumb");
 			
+			$('#caption').html(imageList[myNumber].title);
+			$('#caption').css('display','none').fadeIn(1000);
+			
 			errorLog("Image " + imageList[myNumber].number + " clicked", "white");
 		})
 	})
 	
-	$('#thumbsDiv').css("left", (maxX-leftX-rightX-data.imagelist.length*78)/2 + 'px');
+	//Выставляем размеры DIV-а с thumb-ами
+	$('#thumbsDiv').css("width", data.imagelist.length*78 + 'px'); //!!!!!!!!!
+	$('#thumbsDiv').css("left", (maxX-leftX-rightX-data.imagelist.length*78)/2 + 'px'); ////!!!!
 	
+	//Отображаем картинку из первого thumb-а
 	$('#img0').load( function(){
 		$(this).click();
 	});
 	
+	//При клике по картинке переходим в full-screen режим
 	$('#image').click(function(){
 		var myNumber = $(this).attr('number');
-		$('#topDiv').css('display','block');
+		
+		$('#topImage').css('margin-top', "0px");
 		$('#topImage').attr('src',imageList[myNumber].full_src);
+		$('#topDiv').css('display','block');
 		$('#topImage').css('display','none').fadeIn(1000);
 	})
 	
+	//Позиционируем картинку в full-screen-е по вертикали
+	$('#topImage').load(function(){
+		var iHeight = $(this).height();
+		var dHeight = $('#topDiv').height();
+		
+		if (iHeight < dHeight) {
+			$(this).css('margin-top', (dHeight - iHeight)/2 + "px");
+		}
+	})
+	
+	//При клике по картинке возвращаемся к обычному просмотру
 	$('#topImage').click(function(){
 		$('#topDiv').css('display','block').fadeOut(1000);
 	})
--- zoldatoff/www/zGallery/trunk/php/list_images.php
+++ zoldatoff/www/zGallery/trunk/php/list_images.php
@@ -4,12 +4,14 @@
 		public $norm_src = 'img/norm/00.jpg';
 		public $full_src = 'img/full/00.jpg';
 		public $number = 0;
+		public $title = 'title';
 		
 		function __construct($img_name, $n) {
 			$this->thumb_src = 'img/thumb/' . $img_name;
 			$this->norm_src = 'img/norm/' . $img_name;
 			$this->full_src = 'img/full/' . $img_name;
 			$this->number = $n;
+			$this->title = 'Title ' . $n;
 		}
 	}
 	
