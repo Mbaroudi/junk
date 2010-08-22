eb86f90519de20c4dbca32378fb4725e09ebf3f5
zoldatoff <zoldatoff@8af51d34-cf08-0410-899b-c60a60f868da>
2010-08-22 21:35:12 +0000

    JS partially completed: Folders, Projects & Contexts views are functioning.

--- zoldatoff/www/onlinegtd/js/basic.js
+++ zoldatoff/www/onlinegtd/js/basic.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
 			};
 		});
 	
-	/* */$(".c_text_button[title]").tooltip({ position: "bottom center" });
+	/* $(".c_text_button[title]").tooltip({ position: "bottom center" }); */
 	$(".scrollable").scrollable({ vertical: true, mousewheel: false});	
 	
 	$(".c_input_folder, .c_input_project, .c_input_date")
@@ -41,14 +41,58 @@ $(document).ready(function(){
 	  		}
 		});
 	
-	/* */ $("#ok_inbox").click(function(){
+	$("#folders").click(function(){
+		if ($("#main_menu").data('selected') !== "folders") {
+			$("#main_menu")
+				.data('selected', "folders")
+				.children("li").removeClass("selected");
+			$(this).addClass("selected");
+		}
+		genList1(folders_list, projects_list, {type: "Folders"});
+	});
+	
+	$("#projects").click(function(){
+		if ($("#main_menu").data('selected') !== "projects") {
+			$("#main_menu")
+				.data('selected', "projects")
+				.children("li").removeClass("selected");
+			$(this).addClass("selected");
+		}
+		genList1(projects_list, tasks_list, {type: "Projects"});
+	});
+	
+	$("#contexts").click(function(){
+		if ($("#main_menu").data('selected') !== "contexts") {
+			$("#main_menu")
+				.data('selected', "contexts")
+				.children("li").removeClass("selected");
+			$(this).addClass("selected");
+		}
+		genList1(contexts_list, tasks_list, {type: "Contexts"});
+	});
+	
+	$("#ok_inbox").click(function(){
 		/*var el, vl;
 		vl = $("#input_inbox").val();
 		if (vl !== "Inbox") {
 			el = $("<li>").append(vl);
 			$("ul.c_task_list:first").append(el);
 		}*/
-		$("#folder_list").gen_folderlist($.parseJSON(folder_list));
 	});
+	
+	$("span.c_text_button[title='Delete']").live("click", function(){
+		$(this).parent("li").hide();
+		//TODO: Поправить сам task
+	});
+	
+	$("span.c_text_button[title='Complete']").live("click", function(){
+		var t = $(this);
+		t.siblings().removeClass("c_task_active").removeClass("c_task_skipped");
+		t.siblings(".c_input_level2").addClass("c_task_done");
+		t.siblings(".c_input_date").addClass("c_task_done");
+		//TODO: Поправить сам task
+	});
+	
+	$("#folders").click();
 	
 });
--- zoldatoff/www/onlinegtd/js/gtd.js
+++ zoldatoff/www/onlinegtd/js/gtd.js
@@ -21,6 +21,9 @@ var project_list = '[{\
 "description": "Кухня и ванная",\
 "starred": "1",\
 "status": "active",\
+"start_date": "01.01.2010",\
+"due_date": "31.01.2010",\
+"complete_date": "01.03.2010",\
 "attachments": [],\
 "type": "sequential"\
 },{\
@@ -30,6 +33,9 @@ var project_list = '[{\
 "description": "Books etc.",\
 "starred": "0",\
 "status": "on hold",\
+"start_date": "01.01.2010",\
+"due_date": "31.01.2010",\
+"complete_date": "01.03.2010",\
 "attachments": [],\
 "type": "single actions"\
 },{\
@@ -39,6 +45,21 @@ var project_list = '[{\
 "description": "A site about me...",\
 "starred": "0",\
 "status": "completed",\
+"start_date": "01.01.2010",\
+"due_date": "31.01.2010",\
+"complete_date": "01.03.2010",\
+"attachments": [],\
+"type": "parallel"\
+},{\
+"id": "3",\
+"parent_id": "2",\
+"name": "onlinegtd.ru",\
+"description": "GTD",\
+"starred": "1",\
+"status": "skipped",\
+"start_date": "01.01.2010",\
+"due_date": "31.01.2010",\
+"complete_date": "01.03.2010",\
 "attachments": [],\
 "type": "parallel"\
 }]';
@@ -48,25 +69,25 @@ var task_list = '[{\
 "parent_id": "0",\
 "name": "Купить герметик и водостойкий клей",\
 "description": "3 герметика и 1 клей",\
-"starred": "1",\
+"starred": "0",\
 "status": "active",\
 "attachments": [],\
 "context_id": [0],\
-"start": "01.01.2010",\
-"due": "31.01.2010",\
-"completed": "01.03.2010"\
+"start_date": "01.01.2010",\
+"due_date": "31.01.2010",\
+"complete_date": "01.03.2010"\
 },{\
 "id": "1",\
 "parent_id": "1",\
 "name": "Прочитать документацию по Django",\
 "description": "Oh! Django!",\
-"starred": "0",\
+"starred": "1",\
 "status": "skipped",\
 "attachments": [],\
 "context_id": [1],\
-"start": "01.01.2010",\
-"due": "31.01.2010",\
-"completed": "01.03.2010"\
+"start_date": "01.01.2010",\
+"due_date": "31.01.2010",\
+"complete_date": "01.03.2010"\
 },{\
 "id": "2",\
 "parent_id": "2",\
@@ -76,9 +97,9 @@ var task_list = '[{\
 "status": "completed",\
 "attachments": [],\
 "context_id": [2],\
-"start": "01.01.2010",\
-"due": "31.01.2010",\
-"completed": "01.03.2010"\
+"start_date": "01.01.2010",\
+"due_date": "31.01.2010",\
+"complete_date": "01.03.2010"\
 }]';
 
 var context_list = '[{\
@@ -92,62 +113,146 @@ var context_list = '[{\
 "name": "Home"\
 }]';
 
-$.fn.gen_folderlist = function(flist) {
-	var p_list = Array();
-	var t = $(this).empty();
-	$.each(flist, function(ind, val) {
+var folders_list = $.parseJSON(folder_list);
+var projects_list = $.parseJSON(project_list);
+var tasks_list = $.parseJSON(task_list);
+var contexts_list = $.parseJSON(context_list);
+
+function grepArray(list, p) {
+	return $.grep(list, function(e, i) {
+		var ret = true;
+		
+		if (p.name) ret = ret && (p.name == e.name);		
+		if (p.id) ret = ret && (p.id == e.id);
+		if (p.parent_id) ret = ret && (p.parent_id == e.parent_id);
+		
+		return ret;
+	});
+}
+
+function parseStatus(status) {
+	switch(status) {
+		case "active": 
+			return("c_task_active");
+		case "skipped":	
+			return("c_task_skipped");
+		case "completed":
+			return("c_task_done");
+		default:
+			return("");
+	};
+}
+
+function parseStar(star) {
+	switch(star) {
+		case "1": 
+			return("c_star_button_v");
+		default:
+			return("");
+	};
+}
+
+function switchList1(type) {
+	switch(type) {
+		case "Folders":
+			return(folders_list);
+		case "Projects":
+			return(projects_list);
+		case "Contexts":
+			return(contexts_list);
+		case "Tasks":
+			return(tasks_list);
+		default:
+			return(folders_list);
+	}
+}
+
+function switchList2(type) {
+	switch(type) {
+		case "Folders":
+			return(projects_list);
+		case "Projects":
+			return(tasks_list);
+		case "Contexts":
+			return(tasks_list);
+		default:
+			return(tasks_list);
+	}
+}
+
+function genList1(list1, list2, p) {	
+	var t = $("#main_list").empty();
+	var el;
+	var list, filtered_list;
+	
+	//Список 1-го уровня
+	$.each(list1, function(ind, val) {
 		var el = $("<div>")
 			.append("<span class=c_list_label>▾</span>")
-			//.addClass("c_list_plus")
-			.append("<input type='text' value='" + val.name + "' class='c_input_folder'/>")
-			.append("<ul class='c_project_list'>");
-		
-		p_list = $.grep($.parseJSON(project_list), function(e, i) {
-			return e.parent_id == val.id;
-		});
+			.append("<input type='text' value='" + val.name + "' class='c_input_level1'/>")
+			.append("<ul class='c_level2_list'>");
 			
-		el.children("ul.c_project_list")
-			//.hide()
-			.gen_projectlist(p_list);
+		//Список 2-го уровня
+		list = grepArray(list2, {parent_id: val.id});
+		el.children("ul.c_level2_list").genList2(list);
 		
+		//Раскрытие списка
 		el.children("span.c_list_label").toggle(function() {
-				el.children("ul.c_project_list").show_projectlist();
+				el.children("ul.c_level2_list").show_list2();
 			}, function() {
-				el.children("ul.c_project_list").hide_projectlist();
+				el.children("ul.c_level2_list").hide_list2();
 		});
 		
 		t.append(el);
 	});
 	
-	return t;
-};
+	// Левая панель
+	genLeftList(switchList1(p.type), list2, p);
+}
 
-$.fn.gen_projectlist = function(plist) {
+function genLeftList(list1, list2, p) {	
+	$("#aside_l h2").text(p.type);
+	var ul = $("#left_list").empty();
+	
+	$.each(list1, function(ind, val) {
+		el = $("<li>").text(val.name);
+		el.click(function(){
+			filtered_list = grepArray(list1, {name: val.name});
+			genList1(filtered_list, list2, {type: p.type, selected: val.name});
+		});
+		(val.name == p.selected) && el.addClass("selected");
+		ul.append(el);
+	});
+}
+
+
+$.fn.genList2 = function(list2, p) {
 	var t = $(this).empty();
-	$.each(plist, function(ind, val){
+	
+	$.each(list2, function(ind, val){
 		var el = $("<li>")
-			.append("<span class='c_star_button c_star_button_v'>★ &nbsp;</span>")
-			.append("<input type='text' value='" + val.name + "' class='c_input_project'/>")
-			.append("<input type='text' value='сегодня в 15:00'  class='c_input_date' />");
+			.append("<span class='c_star_button " + parseStar(val.starred) + "'>★ &nbsp;</span>")
+			.append("<input type='text' value='" + val.name + "' class='c_input_level2 " + parseStatus(val.status) + "'/>")
+			.append("<input type='text' value='" + val.due_date + "' class='c_input_date " + parseStatus(val.status) + "' />");
 			
 		var arr = [["Complete", "✓"], ["Delete", "✘"], ["Edit", "➜"], ["Attachment", "⌘"]]; 
 		$.each(arr, function(i, v) {
 			el.append("<span class='c_text_button' title= '" + v[0] + "'>" + v[1] + " &nbsp;</span>");
 		});
 		
+		el.data("id", val.id).data("name", val.name);
 		t.append(el);
 	});
 	
-	//$(".c_text_button[title]").tooltip({ position: "bottom center" });
 	return t;	
 };
 
-$.fn.hide_projectlist = function() {
+$.fn.hide_list2 = function() {
 	$(this).slideDown("fast");
 	$(this).siblings("span.c_list_label").text("▾");
 };
 
-$.fn.show_projectlist = function() {
+$.fn.show_list2 = function() {
 	$(this).slideUp("fast");
 	$(this).siblings("span.c_list_label").text("▸");
 };
