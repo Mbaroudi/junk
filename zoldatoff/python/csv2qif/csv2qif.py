149ee23a7af5c4f14d1ed4bb016361834a80f00b
zoldatoff <zoldatoff@8af51d34-cf08-0410-899b-c60a60f868da>
2012-05-15 20:53:32 +0000

    Рабочая версия - удалось залить транзакции по зарплатке

--- zoldatoff/python/csv2qif/csv2qif.py
+++ zoldatoff/python/csv2qif/csv2qif.py
@@ -7,93 +7,140 @@ decode = 'cp1251'
 #decode = 'utf-8'
 encode = 'utf-8'
 
+accounts = {'7390': {'name': 'Pocket Bank Green', 'type': 'CCard'},
+			'3542': {'name': 'Wage RSB', 'type': 'CCard'}
+			}
+
 def ts2date(ts):
 	year = ts[0:4]
 	month = ts[5:7]
 	day = ts[8:10]
 	return month + '/' + day + '/' + year
 
-def category(instr):
+def genattr(instr):
 	str = instr.upper()
-	ret = ''
+	ret = {'category': 'Другое', 'contragent': ''}
 
-	if str.find('APPLE') 	>= 0: ret = 'Онлайн-покупки: Программы'
-	if str.find('ITUNES')	>= 0: ret = 'Онлайн-покупки: Программы'
+	if 	 str.find('APPLE') 		>= 0: ret = {'category': 'Онлайн-покупки: Программы', 'contragent': 'Apple'}
+	elif str.find('ITUNES')		>= 0: ret = {'category': 'Онлайн-покупки: Программы', 'contragent': 'Apple'}
 
-	if str.find('MEGAFON')	>= 0: ret = 'Счета: Сотовая связь'
-	if str.find('MTS')		>= 0: ret = 'Счета: Сотовая связь'
-	if str.find('BEELINE')	>= 0: ret = 'Счета: Интернет'
-	if str.find('DIPHOST')	>= 0: ret = 'Счета: Другое'
+	elif str.find('MEGAFON')	>= 0: ret = {'category': 'Счета: Сотовая связь', 'contragent': 'Megafon'}
+	elif str.find('MTS')		>= 0: ret = {'category': 'Счета: Сотовая связь', 'contragent': 'MTS'}
+	elif str.find('BEELINE')	>= 0: ret = {'category': 'Счета: Интернет', 'contragent': 'Beeline'}
+	elif str.find('DIPHOST')	>= 0: ret = {'category': 'Счета: Другое', 'contragent': 'Diphost'}
+	elif str.find('MGTS')		>= 0: ret = {'category': 'Счета: Телефон', 'contragent': 'МГТС'}
 
-	if str.find('READABILITY')	>= 0: ret = 'Онлайн-покупки: Программы'
-	if str.find('AMAZON')	>= 0: ret = 'Онлайн-покупки: Программы'
+	elif str.find('READABILITY')>= 0: ret = {'category': 'Онлайн-покупки: Программы', 'contragent': 'Amazon'}
+	elif str.find('AMAZON')		>= 0: ret = {'category': 'Онлайн-покупки: Программы', 'contragent': 'Amazon'}
 
-	if str.find('PAYPAL')	>= 0: ret = 'Онлайн-покупки: Paypal'
+	elif str.find('PAYPAL')		>= 0: ret = {'category': 'Онлайн-покупки: Paypal', 'contragent': 'Paypal'}
 
-	if str.find(u'ПЕРЕВОД')	>= 0: ret = 'Банк: Перевод'
-	if str.find(u'КОМИССИ')	>= 0: ret = 'Банк: Комиссии и штрафы'
-	if str.find(u'ПРОЦЕНТ')	>= 0: ret = 'Банк: Проценты'
+	elif str.find(u'STOLOVAYA')	>= 0: ret = {'category': 'Питание: Столовая', 'contragent': 'Столовая'}
+	elif str.find(u'MCDONALDS')	>= 0: ret = {'category': 'Питание: Рестораны', 'contragent': 'Макдоналдс'}
+	elif str.find(u'TANUKI')	>= 0: ret = {'category': 'Питание: Рестораны', 'contragent': 'Тануки'}
+	elif str.find(u'TEMPL BAR')	>= 0: ret = {'category': 'Питание: Рестораны', 'contragent': 'Темпл-бар'}
+	elif str.find(u'KFC')		>= 0: ret = {'category': 'Питание: Рестораны', 'contragent': 'Ростикс'}
+	elif str.find(u'STARBUCKS')	>= 0: ret = {'category': 'Питание: Рестораны', 'contragent': 'Старбакс'}
+	elif str.find(u'DVE PALOCHKI')	>= 0: ret = {'category': 'Питание: Рестораны', 'contragent': ''}
+	elif str.find(u'SHOKOLADNITSA')	>= 0: ret = {'category': 'Питание: Рестораны', 'contragent': 'Шоколадница'}
 
-	return unicode(ret, encode)
+	elif str.find(u'PEREKRESTOK')	>= 0: ret = {'category': 'Питание: Продукты', 'contragent': 'Перекресток'}
+	elif str.find(u'STANEM DRUZYAMI')	>= 0: ret = {'category': 'Питание: Продукты', 'contragent': 'Станем друзьями'}
+	elif str.find(u'SEDMOY KONTINENT')	>= 0: ret = {'category': 'Питание: Продукты', 'contragent': 'Седьмой континент'}
+	elif str.find(u'KVARTAL')	>= 0: ret = {'category': 'Питание: Продукты', 'contragent': 'Квартал'}
 
-def contragent(instr):
-	str = instr.upper()
-	ret = ''
+	elif str.find(u'OLLGUD')	>= 0: ret = {'category': 'Питание: Другое', 'contragent': ''}
+	elif str.find(u'AROMATNYI MIR')	>= 0: ret = {'category': 'Питание: Другое', 'contragent': ''}
 
-	if str.find('APPLE') 	>= 0: ret = 'Apple'
-	if str.find('ITUNES')	>= 0: ret = 'Apple'
+	elif str.find(u'BELYY VETER')	>= 0: ret = {'category': 'Техника', 'contragent': 'Белый ветер'}
 
-	if str.find('MEGAFON')	>= 0: ret = 'Megafon'
-	if str.find('MTS')		>= 0: ret = 'MTS'
-	if str.find('BEELINE')	>= 0: ret = 'Beeline'
-	if str.find('DIPHOST')	>= 0: ret = 'Diphost'
+	elif str.find(u'GUINOT')	>= 0: ret = {'category': 'Услуги', 'contragent': 'Guinot'}
 
-	if str.find('READABILITY')	>= 0: ret = 'Amazon'
-	if str.find('AMAZON')	>= 0: ret = 'Amazon'
+	elif str.find(u'KOSMIK')	>= 0: ret = {'category': 'Досуг: Развлечения', 'contragent': 'Космик'}
 
-	if str.find('PAYPAL')	>= 0: ret = 'Paypal'
+	elif str.find(u'APTEKA')	>= 0: ret = {'category': 'Здоровье: Аптека', 'contragent': 'Аптека'}
+	elif str.find(u'LINZMASTER')>= 0: ret = {'category': 'Здоровье: Линзы', 'contragent': ''}
 
-	if str.find(u'ПЕРЕВОД')	>= 0: ret = 'Zoldatoff'
-	if str.find(u'КОМИССИ')	>= 0: ret = 'RSB'
-	if str.find(u'ПРОЦЕНТ')	>= 0: ret = 'RSB'
+	elif str.find(u'ATM')		>= 0: ret = {'category': 'Cash', 'contragent': 'Zoldatoff'}
 
-	return unicode(ret, encode)
+	elif str.find(u'ПЕРЕВОД')	>= 0: ret = {'category': 'Перевод', 'contragent': 'Zoldatoff'}
+	elif str.find(u'КОМИССИ')	>= 0: ret = {'category': 'Банк: Комиссии и штрафы', 'contragent': 'RSB'}
+	elif str.find(u'COMISSION')	>= 0: ret = {'category': 'Банк: Комиссии и штрафы', 'contragent': 'RSB'}
+	elif str.find(u'ПРОЦЕНТ')	>= 0: ret = {'category': 'Инвестиции', 'contragent': 'RSB'}
 
+	elif str.find(u'З/ПЛАТЫ')	>= 0: ret = {'category': 'Зарплата', 'contragent': 'RSB'}
+	elif str.find(u'АВАНС')		>= 0: ret = {'category': 'Зарплата', 'contragent': 'RSB'}
 
-def readfile(filename):
+	elif str.find(u'МАТЕРИАЛЬНАЯ')	>= 0: ret = {'category': 'Другое', 'contragent': ''}
+
+	#ret.setdefault('type', 'Bank')
+
+	return {k: unicode(v, encode) for k,v in ret.items()}
+
+def gentrans(colnames, values):
+	trans = dict(zip(colnames, values))
+
+	# incoming or outcoming transaction
+	if trans.pop('type') == '0': trans['amount'] = '-' + trans['amount']
+
+	#date format
+	trans['date'] = ts2date(trans.pop('timestamp'))
+
+	# add category and contragent
+	a = genattr(trans['description'])
+	trans.update(a)
+
+	return trans
+
+def writeacc(accnum, outfile, translist=1):
+	acc = accounts[accnum]
+
+	data = ''
+	if translist == 1: data += '!Account\n'
+	data += 'N' + acc['name'] + '\n'
+	data += 'T' + acc['type'] + '\n'
+	data += '^\n'
+	if translist == 1: data += '!Type:Bank\n'
+
+	outfile.write(data)
+
+def writetrans(trans, outfile):
+	data = ''
+	#data = '!Type:' + trans['type'] + '\n'
+	data += 'D' + trans['date'] + '\n'	
+	data += 'T' + trans['amount'] + '\n'
+	data += 'M' + trans['description'] + '\n'
+	data += 'L' + trans['category'] + '\n'	# L[target account]
+	data += 'P' + trans['contragent'] + '\n'
+	data += '^\n'
+
+	outfile.write(data)
+
+def csv2qif(filename):
 	infile = codecs.open(filename, 'r', decode)
-	outfile = codecs.open('output.qif','w',encode)
+	outfile = codecs.open('output.qif','a+',encode)
 
+	# Write all accounts data
+	#outfile.write('!Account\n')
+	#for accnum in accounts.keys():
+	#	writeacc(accnum, outfile, 0)	
+
+	# Read transactions
 	data = infile.readlines()
-	lines=[line for line in data]
-	# Первая строка содержит названия столбцов
-	colnames=lines[0].strip( ).split(',')[0:]
+	lines = [line for line in data]
+	colnames = lines[0].strip( ).split(',')
+	accnum = lines[1][2:6]
 
-	n = len(colnames)
-
-	print [colnames[r] for r in range(n)]
-
-	outfile.write('!Account\n')
-	outfile.write('NTest\n')
-	outfile.write('TCCard\n')
-	outfile.write('^\n')
-
-	outfile.write('!Type:CCard\n')
+	# Начинаем писать транзакции для нового счета
+	#writeacc(accnum, outfile)
 	for line in lines[1:]:
-		p = line.strip( ).split(',')
-		for r in range(len(p)):
-			if colnames[r] == 'type' and p[r]=='0': sign = '-'
-			if colnames[r] == 'type' and p[r]=='1': sign = ''
+		values = line.strip( ).split(',')
+		trans = gentrans(colnames, values)
+		writetrans(trans, outfile)
 
-			if colnames[r] == 'timestamp': outfile.write('D' + ts2date(p[r]) + '\n')	#date
-			if colnames[r] == 'amount': outfile.write('T' + sign + p[r] + '\n')			#amount
-
-			if colnames[r] == 'description': outfile.write('M' + p[r] + '\n')			#memo
-			if colnames[r] == 'description': outfile.write('L' + category(p[r]) + '\n')	#category
-			if colnames[r] == 'description': outfile.write('P' + contragent(p[r]) + '\n')	#contragent
-		outfile.write('^\n')
-
-		print [p[r] for r in range(len(p))]
-
-#readfile('pocketbank.csv')
-readfile('report.csv')
+outfile = codecs.open('output.qif','w',encode)
+writeacc('3542', outfile)
+csv2qif('63.csv')
+csv2qif('64.csv')
+csv2qif('65.csv')
+csv2qif('66.csv')
