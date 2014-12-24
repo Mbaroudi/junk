d608ea2152f5b95b043b65e5246f73f52e7a90d8
Dmitry Soldatov <zoldatoff@gmail.com>
2014-12-25 01:34:43 +0300

    Удалены конфиги серверов Рубина
    Удалены конфиги серверов Рубина в связи со странной активностью
    Dropbox’а по их синхронизации

--- server/webber.rubin.ru/etc/config-archive/etc/init.d/shutdown.sh
+++
@@ -1,9 +0,0 @@
-# Copyright 1999-2005 Gentoo Foundation
-# Distributed under the terms of the GNU General Public License v2
-# $Header: /var/cvsroot/gentoo-src/rc-scripts/init.d/shutdown.sh,v 1.11.4.3 2005/05/16 23:58:05 vapier Exp $
-
-/sbin/halt -ihdp
-
-# hmm, if the above failed, that's kind of odd ...
-# so let's force a halt
-/sbin/halt -f
