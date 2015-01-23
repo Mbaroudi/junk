8c433f841b5cb3b3c5f0f9f30d7db20b3058076b
Dmitry Soldatov <zoldatoff@gmail.com>
2015-01-23 22:40:52 +0300

    Отобран характерный набор графиков
    Набор графиков
    * распределение ускорений
    * распределение скоростей
    * среднее ускорение при разгоне
    * динамика скорости при разгоне
    * динамика скорости от времени
    * зависимость между скоростью и радиусом поворота

--- zoldatoff/python/driver/driver.py
+++ zoldatoff/python/driver/driver.py
@@ -12,20 +12,23 @@ The intent of this competition is to develop an algorithmic signature
 of driving type.
 """
 
+import os
+import random
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 # Откуда и сколко траекторий берём
 DATA_PATH = '/Users/zoldatoff/Downloads/driver/data/'
-DRIVER_NUM = 10
-NUM_CSV = 10
+DRIVER_NUM = 1
+NUM_CSV = 30
 
-
-SHIFT_LEN = 1       # Усреднение по SHIFT_LEN точкам при расчёте скоорости
-VELOCITY_LOW = 1    # Участок ускорения отбирается для скоростей из диапазона
-VELOCITY_HIGH = 15  # между VELOCITY_LOW и VELOCITY_HIGH
+VELOCITY_LOW = 5    # Участок ускорения отбирается для скоростей из диапазона
+VELOCITY_HIGH = 15  #       между VELOCITY_LOW и VELOCITY_HIGH
 ACCEL_MINLEN = 15   # Длина участка разгона не менее ACCEL_MINLEN точек
+#ACCEL_HEAD = 5     # В начале разгона ускорение завышено - этот участок мы отбрасываем
+ACCEL_TAIL = 3      # В конце разгона ускорение снижается - этот участок мы отбрасываем
 
 
 
@@ -38,30 +41,31 @@ def csv2dataframe(filename=DATA_PATH + '/1/1.csv'):
 
     # pylint: disable=E1103
     data['t'] = data.index                                              # время
-    data['dt'] = data['t'] - data['t'].shift(SHIFT_LEN)                 # дельта времени
-    data['ds'] = sum([                                                  # расстояние
-        np.sqrt(
-            np.square(data['x'].shift(shift) - data['x'].shift(shift+1))
-            +
-            np.square(data['y'].shift(shift) - data['y'].shift(shift+1))
-        )
-        for shift in range(SHIFT_LEN)
-    ])
+    data['dt'] = data['t'] - data['t'].shift(1)                         # дельта времени
+    data['ds'] = np.sqrt(                                               # расстояние
+        np.square(data['x'] - data['x'].shift(1))
+        +
+        np.square(data['y'] - data['y'].shift(1))
+    )
     data['v'] = data['ds'] / data['dt']                                 # скорость
-    data['a'] = (data['v'] - data['v'].shift(SHIFT_LEN)) / data['dt']   # ускорение
+    data['a'] = (data['v'] - data['v'].shift(1)) / data['dt']           # ускорение
     data['ang'] = np.where(                                             # направление движения
-        data['ds'] > 0.0,
+        data['ds'] > 0.5,
         np.arctan2(
             data['y']-data['y'].shift(1),
             data['x']-data['x'].shift(1)
         ),
-        0.0
+        None
     )
+
     data['dang'] = data['ang'] - data['ang'].shift(1)                   # смена направления движения
-    data['r'] = data['ds'] / (abs(data['dang']) + 0.000001)             # радиус поворота
+    data['dang'] = data['dang'].apply(lambda x: x - 2.0*np.pi if x >  4.0 else x)
+    data['dang'] = data['dang'].apply(lambda x: x + 2.0*np.pi if x < -4.0 else x)
+
+    data['r'] = (data['ds'] + data['ds'].shift(1)) / (abs(data['dang']) + 0.000001)             # радиус поворота
     # pylint: enable=E1103
 
-    #print data[['x', 'y', 'ang']]
+    #print data[['x', 'y', 'ds', 'v', 'ang', 'dang', 'r']]
 
     return data
 
@@ -88,7 +92,8 @@ def extract_accel(data):
             accel.append((i, curr['v'], curr['a']))
 
         elif len(accel) > ACCEL_MINLEN and curr['a'] < 0.0 and prev['a'] < 0.0:
-            accel_all.append(pd.DataFrame(accel, columns=['t', 'v', 'a']))
+            df = pd.DataFrame(accel, columns=['t', 'v', 'a'])
+            accel_all.append(df[0:-ACCEL_TAIL])
             accel = []
 
         else:
@@ -107,64 +112,88 @@ def plot_stats():
     'Driver Identification by Driving Style'
     """
 
-    for i in range(NUM_CSV):
-        driver_data = csv2dataframe(DATA_PATH + '/' + str(DRIVER_NUM) + '/' + str(i+1) + '.csv')
+    path = DATA_PATH + '/' + str(DRIVER_NUM) + '/'
+    files = os.listdir(path)
+
+    for file in random.sample(files, NUM_CSV):
+        driver_data = csv2dataframe(path + file)
 
         accel_all = extract_accel(driver_data)
+
+        accel_a = []
 
         for accel in accel_all:
             plt.figure(4)
             accel['v'].plot()
 
-            plt.figure(5)
-            accel['a'].hist(bins=80, normed=True)
+            accel_a.append(accel['a'].mean())
 
+        plt.figure(5)
+        if accel_a:
+            plt.hist(accel_a, range=(0, 5), bins=40, normed=True)
 
         fig = plt.figure(1)
         driver_data.plot(x='x', y='y', ls='--', ax=fig.gca()) #color='red', markerfacecolor='red',
 
         plt.figure(2)
-        driver_data['v'].hist(bins=40, range=(1, 41), normed=True)
+        driver_data['v'].hist(bins=40, normed=True)
 
         plt.figure(3)
-        driver_data['a'].hist(bins=40, range=(0.1, 5.0), normed=True)
+        driver_data['a'].hist(bins=40, normed=True)
 
         fig = plt.figure(6)
         driver_data.plot(x='r', y='v', ls='', marker='.', ax=fig.gca())
 
-        fig = plt.figure(7)
-        driver_data.plot(x='t', y='ang', ax=fig.gca())
+        #fig = plt.figure(7)
+        #driver_data.plot(x='t', y='ang', ax=fig.gca())
 
-        print 'driver =', DRIVER_NUM, 'traj =', i, ' accel_cnt = ', len(accel_all)
+        fig = plt.figure(8)
+        driver_data.plot(x='t', y='v', ax=fig.gca())
+
+        print 'driver =', DRIVER_NUM, 'file =', file, ' accel_cnt = ', len(accel_all)
 
     plt.figure(1)
     plt.autoscale()
-    #plt.legend().remove() #(ncol=3, loc='lower right', fontsize=7)
-    plt.savefig('trajectory.eps')
+    plt.legend().remove() #(ncol=3, loc='lower right', fontsize=7)
+    plt.savefig(str(DRIVER_NUM) + '. trajectory.eps')
 
     plt.figure(2)
-    plt.savefig('velocity.eps')
+    plt.xlim([1, 45])
+    plt.ylim([0, 0.5])
+    plt.savefig(str(DRIVER_NUM) + '. v(hist).eps')
 
     plt.figure(3)
-    plt.savefig('acceleration.eps')
+    plt.xlim([0.2, 6])
+    plt.ylim([0, 1])
+    plt.savefig(str(DRIVER_NUM) + '. a(hist).eps')
 
     plt.figure(4)
-    plt.savefig('accel_v.eps')
+    plt.xlim([0, 45])
+    plt.ylim([0, 30])
+    plt.savefig(str(DRIVER_NUM) + '. accel v(t).eps')
 
     plt.figure(5)
-    plt.xlim([0.01, 4.0])
-    plt.savefig('accel_a.eps')
+    plt.xlim([0.01, 4])
+    plt.ylim([0, 10])
+    plt.savefig(str(DRIVER_NUM) + '. accel mean(a).eps')
 
     plt.figure(6)
-    plt.autoscale()
-    plt.xlim([0.01, 10.0])
-    plt.ylim([0.01, 10.0])
-    plt.savefig('r_v.eps')
+    plt.legend().remove()
+    plt.xlim([0, 10])
+    plt.ylim([0, 10])
+    plt.savefig(str(DRIVER_NUM) + '. v(r).eps')
 
-    plt.figure(7)
-    plt.autoscale()
-    plt.xlim([0.01, 110.0])
-    plt.savefig('angle.eps')
+    #plt.figure(7)
+    #plt.legend().remove()
+    #plt.xlim([0, 50])
+    #plt.ylim([-4, 4])
+    #plt.savefig(str(DRIVER_NUM) + '. ang(t).eps')
+
+    plt.figure(8)
+    plt.legend().remove()
+    plt.xlim([0, 500])
+    plt.ylim([0, 30])
+    plt.savefig(str(DRIVER_NUM) + '. v(t).eps')
 
 
 plot_stats()
