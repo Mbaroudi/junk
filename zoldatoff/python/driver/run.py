5e1170e7abc763334f8842f76e0cb816926e7aff
Dmitry Soldatov <zoldatoff@gmail.com>
2015-03-29 18:36:37 +0300

    Попытка №26
    Коэффициент gamma снижен с 0.05 до 0.025

--- zoldatoff/python/driver/run.py
+++ zoldatoff/python/driver/run.py
@@ -90,7 +90,7 @@ def apply_clf(files, main_driver=1, classify='SVM'):
     if classify == 'SVM':
         clf = svm.SVC(C=0.1,
                       kernel='rbf',
-                      gamma=0.05,
+                      gamma=0.025,
                       shrinking=True,
                       verbose=False)
     elif classify == 'RFC':
